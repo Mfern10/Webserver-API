2e1ccb8de972183404d16684cfd0f8d1c8aed2fc
Mfern10 <mitchellfernandez147@gmail.com>
2023-12-06 15:55:16 +0900

    Created main arcitecture and connected database to app.py

---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://Roflgank3d:abc123@127.0.0.1:5432/web_server_api'
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return "success"
+
+if __name__ == '__main__':
+    app.run(debug=True)
