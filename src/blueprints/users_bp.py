dc83bc5713da83184d3ab62fd33b9a9f28d9c582
Mfern10 <mitchellfernandez147@gmail.com>
2023-12-07 15:56:36 +0900

    Added get all users Endpoint to users blueprint

--- src/blueprints/users_bp.py
+++ src/blueprints/users_bp.py
@@ -1,5 +1,5 @@
 from setup import bcrypt, db
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from models.user import UserSchema, User
 from sqlalchemy.exc import IntegrityError
 
@@ -24,5 +24,21 @@ def register():
         return UserSchema(exclude=['password']).dump(user), 201
     except IntegrityError:
         return {'error': 'Email address is already being used'}, 409
+    
+# gets all users ** make sure it is authroized for admins only **
+@users_bp.route('/', methods=['GET'])
+def all_users():
+    # use query to retrieve all users from the database, Using query.all() instead of scalar
+    # as scalar is better for a single user but here I want to retrieve all users from the database.
+    users = User.query.all()
+    
+    # Serialize the users using Marshamllow schema making sure to exclude passwords
+    users_schema = UserSchema(many=True, exclude=['password'])
+    serialized_users = users_schema.dump(users)
+
+    # Return the serialized users as a JSON response
+    return jsonify(serialized_users), 200
+
+
 
 
