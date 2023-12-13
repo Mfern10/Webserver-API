a12bb5178396782620ef0a90553862d5be1638f1
Mfern10 <mitchellfernandez147@gmail.com>
2023-12-13 09:58:49 +0900

    Updated comments in products blueprint to be more concise yet informative, added some generic error handlers to app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,29 @@
 from setup import app
+from flask import jsonify
 from blueprints.commands_bp import db_commands
 from blueprints.users_bp import users_bp
 from blueprints.categories_bp import categories_bp
 from blueprints.products_bp import products_bp
 from blueprints.reviews_bp import reviews_bp
+from marshmallow import ValidationError
 
+
+# Blueprint Registry
 app.register_blueprint(db_commands)
 app.register_blueprint(users_bp)
 app.register_blueprint(categories_bp)
 app.register_blueprint(products_bp)
 app.register_blueprint(reviews_bp)
+
+
+# Generic error handling for application
+@app.errorhandler(ValidationError)
+def validation_error_handler(error):
+    return jsonify.error({'error': error.messages}), 400
+
+@app.errorhandler(Exception)
+def unexpected_error_handler(error):
+    app.logger.error(f"An unexpected error occurred: {error}")
+    return jsonify({'error': 'An unexpected error occurred'}), 500
+
+
--- src/blueprints/products_bp.py
+++ src/blueprints/products_bp.py
@@ -13,39 +13,49 @@ products_bp = Blueprint('products_bp', __name__, url_prefix='/products')
 
 @products_bp.route('/', methods=['GET'])
 def all_products():
+    # Retrieving all prducts from the database
     products = Product.query.all()
 
+    # Serializing the products with ProductSchema
     products_schema = ProductSchema(many=True)
     serialized_products = products_schema.dump(products)
 
+    # Returns the list of products as JSON
     return jsonify(serialized_products), 200
 
 
 @products_bp.route('/<int:product_id>', methods=['GET'])
 def get_product(product_id):
-    # use stmt db.select query with scalar to select a specific category from the table,
-    # as long as ID matches all details of category will be returned as no security issues.
-    # if id is not in system will return a 404 error
+    # Select query for specific product by ID
     stmt = db.select(Product).filter_by(id=product_id)
-    category = db.session.scalar(stmt)
-    if category:
-        return ProductSchema().dump(category)
+    product = db.session.scalar(stmt)
+
+    # Handling the retrieved product data
+    if product:
+        # Serialize the retrieved product using ProductSchema 
+        # Return as JSON
+        return ProductSchema().dump(product)
     else:
-        return {'error': 'Product not found'}, 404
+        return {'error': 'Product not found'}, 404 # Returning 404 if product not found
 
 
 @products_bp.route('/', methods=['POST'])
 @jwt_required()
 def new_product():
     try:
+        # obtaining the current users identity from JWT
         current_user_id = get_jwt_identity()
+
+        # Loads and validates the user provided data with ProductSchema
         product_info = ProductSchema(exclude=['id']).load(request.json)
 
+        # Check if the product with the same name already exists
         existing_product = Product.query.filter_by(
             name=product_info['name']).first()
         if existing_product:
             return jsonify({'error': 'Product already exists'}), 400
 
+        # Creating a a new product object  using user provided data
         product = Product(
             name=product_info['name'],
             description=product_info['description'],
@@ -56,10 +66,14 @@ def new_product():
             date_created=datetime.now()  # Set date_created to the current timestamp
         )
 
+        # Adding new product to the database
         db.session.add(product)
         db.session.commit()
 
+        # Returns newly created product as JSON
         return ProductSchema().dump(product), 201
+    
+    # Shows Integreity 500 if error occurs
     except IntegrityError:
         return jsonify({'error': 'Integrity error occured while creating product'}), 500
 
@@ -67,12 +81,20 @@ def new_product():
 @products_bp.route('/<int:product_id>', methods=['PUT', 'PATCH'])
 @jwt_required()
 def update_product(product_id):
+    # Loading and validating the user provided data using ProductSchema
     product_info = ProductSchema(
         exclude=['id', 'user_id', 'date_created']).load(request.json)
+    
+    # Uses Selecy to query the specific product by its ID
     stmt = db.select(Product).filter_by(id=product_id)
     product = db.session.scalar(stmt)
+
+    # Handling the retrived product data
     if product:
         authorize(product.user_id)
+
+        # Updating the product details if they are provided in the request,
+        # Else keep existing values
         product.name = product_info.get('name', product.name)
         product.description = product_info.get(
             'description', product.description)
@@ -80,21 +102,32 @@ def update_product(product_id):
         product.color = product_info.get('color', product.color)
         product.category_id = product_info.get(
             'category_id', product.category_id)
+        
+        # Commiting the changes to the product to the database
         db.session.commit()
+
+        # Returns the updated product details as JSON
         return ProductSchema().dump(product)
     else:
-        return {'error': 'Product not found'}, 401
+        return {'error': 'Product not found'}, 404 # Returns 401 if ID does not exist
 
 
 @products_bp.route('/<int:product_id>', methods=['DELETE'])
 @jwt_required()
 def delete_product(product_id):
+    # Select query to get the specific product by its ID
     stmt = db.select(Product).filter_by(id=product_id)
     product = db.session.scalar(stmt)
+
+    # Handling the Retrieved data
     if product:
-        authorize()
+        authorize() # Autorizes the delete action if logic met
+
+        # Delete the product and commit to the database
         db.session.delete(product)
         db.session.commit()
+
+        # Returning a message indicating successful delete
         return ({'message': 'Product deleted successfully'}), 200
     else:
-        return {'error': 'Product not found'}, 404
+        return {'error': 'Product not found'}, 404 # Returns error 404 if ID does not exist
